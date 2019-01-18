d358a15ed57f3984b598252d7e350d2e49f136cf
Jorgen Azure Windows <jorgent@digitalvaluenetwork.com>
2019-01-18 08:18:21 +0000

    Adapted logic to actually work on Windows machine.

--- server/runQuery.ts
+++ server/runQuery.ts
@@ -1,16 +1,37 @@
-import { Connection, TYPES, Request } from 'tedious';
+import { Connection, TYPES, Request, ConnectionConfig } from 'tedious';
+import { ETIME } from 'constants';
+import { exists } from 'fs';
 
-export const runCommand = (cmd: { sql: string, params: {[index: string]: any } }) => {
-	const config = {
-		userName: 'sa', // update me
-		password: 'your_password', // update me
-		server: 'localhost',
-		options: {
-			database: 'SampleDB'
-		}
+const config: ConnectionConfig = {
+	userName: process.env.username,
+	password: process.env.password,
+	server: process.env.server || 'sqlservertest',
+	options: {
+		database: process.env.database || 'XMLTest',
 	}
+}
 
-	const connection = new Connection(config);
+const connection = new Connection(config);
+
+connection.on('connect', function (err) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('Connected');
+	}
+});
+
+const exitOnFailure = () => {
+	console.log("exiting due to lost or failed connection")
+	process.exit(1)
+}
+// These are not really necessary, as the application will fail by the unhandled exception anyways.
+connection.on('end', exitOnFailure)
+connection.on('error', exitOnFailure)
+
+
+export const runCommand = (cmd: { sql: string, params: { [index: string]: any } }) => {
+
 
 	const result = new Promise<number>((acc, rej) => {
 		const request = new Request(
@@ -24,8 +45,9 @@ export const runCommand = (cmd: { sql: string, params: {[index: string]: any } }
 			});
 		for (const key of Object.keys(cmd.params)) {
 			const value = cmd.params[key]
+			// XML type does not work, uses nvvarchar anyways, which fortunately works.
 			const sqlServerType = typeof value === "string" ?
-						(value.indexOf("<") === 0 ? TYPES.Xml : TYPES.NVarChar) : TYPES.Numeric
+				(value.indexOf("<") === 0 ? TYPES.NVarChar : TYPES.NVarChar) : TYPES.Numeric
 			request.addParameter(key, sqlServerType, value)
 		}
 
